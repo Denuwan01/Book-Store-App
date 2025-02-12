88211dcc58e30e04b66af0d9b81c3e350e491e84
Denuwan Nammuni <denuwannammuni@gmail.com>
2025-02-12 22:17:46 +0530

    Updates

--- Frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; // if your file is App.jsx
-// Ensure this path matches your actual App.js file location
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
